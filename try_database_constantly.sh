834763c367d928c9ddad0a5726cd8d3d01313e4d
Farley <andrew@neonsurge.com>
2016-06-27 02:39:20 +0200

    updating try

--- try_database_constantly.sh
+++ try_database_constantly.sh
@@ -5,6 +5,7 @@
 HOSTNAME=demo-for-autoscaling-707880566.eu-west-1.elb.amazonaws.com
 
 while true; do 
-    curl http://$HOSTNAME/database.php
-    sleep 0.2
+    curl --silent http://$HOSTNAME/database.php | grep -i "rows" | awk '{print $(NF-5),"\t",$NF}'
+
+    sleep 0.5
 done
